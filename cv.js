79814247c099787e363cefa6a615083a99e4261b
CandelarioRamosj <145868728+CandelarioRamosj@users.noreply.github.com>
2023-10-14 21:35:33 -0600

    se agrego el cv y el script para abrirlo

---
+++ cv.js
@@ -0,0 +1,5 @@
+let button = document.getElementById('button-cv')
+
+button.addEventListener('click', function(){
+  window.open('pdf/CV-Web-CandelarioRamosJimenez.pdf', '_blank', 'noopener,noreferrer')
+})
