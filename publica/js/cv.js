ffecc9461e35a2f739bb9a24e68de2fa52c74276
CandelarioRamosj <145868728+CandelarioRamosj@users.noreply.github.com>
2023-10-17 18:30:43 -0600

    actualizar el orden de los archivos

--- publica/js/cv.js
+++ publica/js/cv.js
@@ -1,5 +1,5 @@
 let button = document.getElementById('button-cv')
 
 button.addEventListener('click', function(){
-  window.open('pdf/CV-Web-CandelarioRamosJimenez.pdf', '_blank', 'noopener,noreferrer')
+  window.open('publica/pdf/CV-Web-CandelarioRamosJimenez.pdf', '_blank', 'noopener,noreferrer')
 })
